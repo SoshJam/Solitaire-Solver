98b102c3e4c9c6004b170386abb3f11c5eb20db6
Sam Hill <soshjam@soshjam.com>
2023-03-21 20:22:38 -0600

    Formatting change

--- Solitaire/Card.cs
+++ Solitaire/Card.cs
@@ -1,29 +1,32 @@
-﻿namespace Solitaire
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Solitaire
 {
-	public struct Card
-	{
-		public Suit suit { get; private set; }
-		public int value { get; private set; }
+    public struct Card
+    {
+        public Suit suit { get; private set; }
+        public int value { get; private set; }
 
-		public bool IsBlack
-		{
-			get { return suit == Suit.Spades || suit == Suit.Clubs; }
-			private set { IsBlack = value; } // Updating this will not do anything.
-		}
+        public bool IsBlack
+        {
+            get { return suit == Suit.Spades || suit == Suit.Clubs; }
+            private set { IsBlack = value; } // Updating this will not do anything.
+        }
 
-		public Card(Suit suit, int value)
-		{
-			this.suit = suit;
-			this.value = value;
-		}
+        public Card(Suit suit, int value)
+        {
+            this.suit = suit;
+            this.value = value;
+        }
 
-		public override string ToString()
-		{
-			string valueString = "";
-			switch (value) {
-				case 1:
-					valueString = "Ace";
-					break;
+        public override string ToString()
+        {
+            string valueString = "";
+            switch (value)
+            {
+                case 1:
+                    valueString = "Ace";
+                    break;
                 case 11:
                     valueString = "Jack";
                     break;
@@ -33,20 +36,20 @@
                 case 13:
                     valueString = "King";
                     break;
-				default:
-					valueString = value.ToString();
-					break;
+                default:
+                    valueString = value.ToString();
+                    break;
             }
 
-			return $"{valueString} of {suit.ToString()}";
-		}
-	}
+            return $"{valueString} of {suit.ToString()}";
+        }
+    }
 
-	public enum Suit
-	{
-		Spades,
-		Diamonds,
-		Clubs,
-		Hearts
-	}
+    public enum Suit
+    {
+        Spades,
+        Diamonds,
+        Clubs,
+        Hearts
+    }
 }
