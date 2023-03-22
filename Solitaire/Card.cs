68de3235a34540fab99173639631b8d357a567bd
Sam Hill <soshjam@soshjam.com>
2023-03-21 19:38:26 -0600

    Add project files.

---
+++ Solitaire/Card.cs
@@ -0,0 +1,52 @@
+﻿namespace Solitaire
+{
+	public struct Card
+	{
+		public Suit suit { get; private set; }
+		public int value { get; private set; }
+
+		public bool IsBlack
+		{
+			get { return suit == Suit.Spades || suit == Suit.Clubs; }
+			private set { IsBlack = value; } // Updating this will not do anything.
+		}
+
+		public Card(Suit suit, int value)
+		{
+			this.suit = suit;
+			this.value = value;
+		}
+
+		public override string ToString()
+		{
+			string valueString = "";
+			switch (value) {
+				case 1:
+					valueString = "Ace";
+					break;
+                case 11:
+                    valueString = "Jack";
+                    break;
+                case 12:
+                    valueString = "Queen";
+                    break;
+                case 13:
+                    valueString = "King";
+                    break;
+				default:
+					valueString = value.ToString();
+					break;
+            }
+
+			return $"{valueString} of {suit.ToString()}";
+		}
+	}
+
+	public enum Suit
+	{
+		Spades,
+		Diamonds,
+		Clubs,
+		Hearts
+	}
+}
---
+++ Solitaire/GameState.cs
@@ -0,0 +1,22 @@
+﻿namespace Solitaire
+{
+    public class GameState
+    {
+        /// <summary>
+        /// The cards that are known to be face-up and in play, either on the
+        /// board or in the foundation piles.
+        /// </summary>
+        public HashSet<Card> CardsInPlay { get; private set; }
+
+        /// <summary>
+        /// The facedown cards in the Stock Pile. There are 24 to start.
+        /// </summary>
+        public Queue<Card> StockPile { get; private set; }
+
+        /// <summary>
+        /// The faceup cards in the Waste Pile. When the StockPile is empty,
+        /// this becomes the new StockPile and is replaced with a new Queue.
+        /// </summary>
+        public Queue<Card> WastePile { get; private set; }
+    }
+}
